bf4f5a7221deffbbb8d3e61d1dd858f0fd7e3ecd
Farrel Rabbani Gunawan <farrelrabbanigunawan@gmail.com>
2024-01-29 13:03:27 +0700

    Fungsi

---
+++ fungsi.js
@@ -0,0 +1,13 @@
+var x = "25";
+var y = "3.14";
+
+var toNum = parseInt(x);
+var toFloat = parseFloat(y);
+var toString = toNum.toString();
+console.log(toString + toString);
+
+function jumlah(a, b) {
+    return a + b;
+}
+
+alert(jumlah(5, 10));
