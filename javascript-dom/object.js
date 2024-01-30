1da2482ab7fc4c570318c6a617c998994646ef76
Farrel Rabbani Gunawan <farrelrabbanigunawan@gmail.com>
2024-01-30 09:00:21 +0700

    Object-Dom

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -6,7 +6,18 @@ siswa.namaLengkap = function () {
     return this.namaDepan + " " + this.namaBelakang;
 };
 
+var siswa2 = {
+    namaDepan: "Qeyra",
+    namaBelakang: "Azzahra",
+    alamat: "Cijagra",
+    namaLengkap: function () {
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+    
+};
+
 alert("Nama :" + siswa.namaLengkap());
+alert("Nama :" + siswa2.namaLengkap());
 
 // function namaLengkap() {
 
